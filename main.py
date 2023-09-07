d25fcaa8b9029a6648d302edfd7e2a1167e557eb
G Bharath Sandilya <sandilya1599@gmail.com>
2023-09-07 23:19:27 +0530

    Fix lint errors.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from image_handler.image_handler import ImageHandler
 from vid_handler.frame_extraction import VideoMerger
 
-v = VideoMerger("samples/base.mp4", "samples/secret.mp4", 1)
+v = VideoMerger("samples/videos/base.mp4", "samples/videos/secret.mp4", 1)
 v.encode_video()
 v.decode_video()
 
 im = ImageHandler(2)
 im.encode_image("samples/images/high_res/secret_highres.png", "samples/images/high_res/secret_yn.png")
-im.decode_image("samples/output.png")
+im.decode_image("samples/images/high_res/secret_highres.png")
