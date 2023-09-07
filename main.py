3b201fa4c187074802badf8e7b1989c684721c13
G Bharath Sandilya <sandilya1599@gmail.com>
2023-09-07 23:11:26 +0530

    update for image handling

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from image_handler.image_handler import ImageHandler
 from vid_handler.frame_extraction import VideoMerger
 
 v = VideoMerger("samples/base.mp4", "samples/secret.mp4", 1)
 v.encode_video()
-# v.decode_video()
+v.decode_video()
+
+im = ImageHandler(2)
+im.encode_image("samples/images/high_res/secret_highres.png", "samples/images/high_res/secret_yn.png")
+im.decode_image("samples/output.png")
