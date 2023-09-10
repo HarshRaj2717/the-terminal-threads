22f0d10a6f0502ba25f1b20fa9bf185eb936ca13
slimeborger <sujaylobo05@gmail.com>
2023-09-10 19:47:08 +1000

    reverted the dumb changes i made

--- encrypt/encrypt.py
+++ encrypt/encrypt.py
@@ -1,15 +1,12 @@
 import numpy as np
 import cv2
 
-def encrypt_frame(mask_imgpath: str, secret_imgpath: str,output_path:str, bitcount: int = 3, secret_code: int = 1,) -> np.ndarray:
+def encrypt_frame(mask_array: np.ndarray, secret_array: np.ndarray,secret_code: int = 1, bitcount: int = 3) -> np.ndarray:
     """
     Encrypt the `secret_image` using `key` and hide it inside `mask_image`
 
     Returns the updated `mask_image`
     """
-
-    mask_array = cv2.imread(mask_imgpath, flags=1)
-    secret_array = cv2.imread(secret_imgpath, flags=1)
 
     # If the mask image is bigger than secret image, take a subsection (top-left corner)
     if mask_array.size > secret_array.size:
@@ -26,5 +23,7 @@ def encrypt_frame(mask_imgpath: str, secret_imgpath: str,output_path:str, bitcou
     secret_array = secret_array >> (8 - bitcount)
     mask_array = (mask_array >> bitcount) << bitcount
     mask_array = mask_array | secret_array
-    cv2.imwrite(output_path,mask_array)
+
     return mask_array
+
+
--- encrypt/encrypt_test.py
+++ encrypt/encrypt_test.py
@@ -1,7 +1,14 @@
 import encrypt
+import cv2
 
 MASK_IMAGE_PATH = "samples/images/high_res/mask_highres.png"
 SECRET_IMAGE_PATH = "samples/images/high_res/secret_highres.png"
 SECRET_CODE = 1
 
-encrypted_array = encrypt.encrypt_frame(MASK_IMAGE_PATH,SECRET_IMAGE_PATH,"samples\output.png")
+mask_array = cv2.imread(MASK_IMAGE_PATH, flags=1)
+secret_array = cv2.imread(SECRET_IMAGE_PATH, flags=1)
+
+
+encrypted_array = encrypt.encrypt_frame(MASK_IMAGE_PATH,SECRET_IMAGE_PATH,SECRET_CODE)
+
+cv2.imwrite("samples/output.png", encrypted_array)
